26fa5f1a743e916f3a60765ddae80cbc1d9982b1
Michel Bernardo <bernardoenglish@outlook.com>
2020-08-14 17:41:27 -0400

    refactoring function to handle deleting question

--- gym_management/public/scripts.js
+++ gym_management/public/scripts.js
@@ -1,8 +1,17 @@
 const currentPage = location.pathname;
 const menuItems = document.querySelectorAll("header .links a");
+const formDelete = document.querySelector("#form-delete");
+
 
 for (item of menuItems) {
     if (currentPage.includes(item.getAttribute("href"))) {
         item.classList.add("active");
     }
 }
+
+formDelete.addEventListener("submit", function () {
+    const confirmation = confirm("Do you want to delete?")
+    if (!confirmation) {
+        event.preventDefault()
+    }
+})
