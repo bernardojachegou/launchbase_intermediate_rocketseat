9504df76179d2f82ebd5a6e4b4815066648079ed
Michel Bernardo <bernardoenglish@outlook.com>
2020-08-17 22:55:35 -0400

    adding frontend pagination logic

--- gym_management/public/scripts.js
+++ gym_management/public/scripts.js
@@ -2,7 +2,6 @@ const currentPage = location.pathname;
 const menuItems = document.querySelectorAll("header .links a");
 const formDelete = document.querySelector("#form-delete");
 
-
 for (item of menuItems) {
     if (currentPage.includes(item.getAttribute("href"))) {
         item.classList.add("active");
@@ -15,3 +14,46 @@ formDelete.addEventListener("submit", function () {
         event.preventDefault()
     }
 })
+
+function paginate(selectedPage, totalPages) {
+
+    let totalPages = 20,
+        selectedPage = 15,
+        pages = []
+    oldPage
+
+    for (let currentPage = 0; currentPage <= totalPages; currentPage++) {
+
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+        if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+            pages.push(currentPage)
+
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push('...')
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(currentPage - 1)
+            }
+
+            oldPage = currentPage
+        }
+    }
+    return pages
+}
+
+const pagination = document.querySelector(".pagination")
+const page = +pagination.dataset.page;
+const total = +pagination.dataset.total;
+const pages = paginate(page, total);
+
+let elements = ""
+
+for (let page of pages) {
+    elements += `<a href="#">${page}</a>`
+}
+
+pagination.innerHTML = elements;
--- gym_management/src/app/controllers/instructors.js
+++ gym_management/src/app/controllers/instructors.js
@@ -15,21 +15,19 @@ module.exports = {
             limit,
             offset,
             callback(instructors) {
-                return response.render("instructors/index", { instructors, filter })
+
+                const pagination = {
+                    total: Math.ceil(instructors[0].total / limit),
+                    page
+                }
+
+                return response.render("instructors/index", { instructors, pagination, filter })
             }
         }
 
         Instructor.paginate(params)
 
-        // if (filter) {
-        //     Instructor.findBy(filter, function (instructors) {
-        //         return response.render("instructors/index", { instructors, filter })
-        //     })
-        // } else {
-        //     Instructor.all(function (instructors) {
-        //         return response.render("instructors/index", { instructors })
-        //     })
-        // }
+
     },
 
     create(request, response) {
