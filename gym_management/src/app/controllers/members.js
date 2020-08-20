cb89f37a0f99740d98e4cb332ee357bb2e86b971
Michel Bernardo <bernardoenglish@outlook.com>
2020-08-19 20:26:03 -0400

    pagination style and filter configuration done

--- gym_management/src/app/controllers/members.js
+++ gym_management/src/app/controllers/members.js
@@ -4,10 +4,29 @@ const { age, date } = require('../../lib/utils');
 
 module.exports = {
     index(request, response) {
+        let { filter, page, limit } = request.query
 
-        Member.all(function (members) {
-            return response.render("members/index", { members })
-        })
+        page = page || 1
+        limit = limit || 2
+        let offset = limit * (page - 1)
+
+        const params = {
+            filter,
+            page,
+            limit,
+            offset,
+            callback(members) {
+
+                const pagination = {
+                    total: Math.ceil(members[0].total / limit),
+                    page
+                }
+
+                return response.render("members/index", { members, pagination, filter })
+            }
+        }
+
+        Member.paginate(params)
     },
 
     create(request, response) {
--- gym_management/src/app/models/member.js
+++ gym_management/src/app/models/member.js
@@ -108,6 +108,46 @@ module.exports = {
 
             callback(results.rows);
         })
+    },
+    paginate(params) {
+        const { filter, limit, offset, callback } = params
+
+        let query = "",
+            filterQuery = "",
+            totalQuery = `(
+                SELECT count(*) FROM members
+                ) AS total
+            `
+
+
+        if (filter) {
+
+            filterQuery = `
+                WHERE members.name ILIKE '%${filter}%'
+                OR members.email ILIKE '%${filter}%'
+
+            `
+
+            totalQuery = `(
+                SELECT count(*) from members
+                ${filterQuery}
+                ) AS total
+            `
+        }
+
+
+        query = `
+            SELECT members.*, ${totalQuery}
+            FROM members
+            ${filterQuery}
+            LIMIT $1 OFFSET $2
+        `
+
+        db.query(query, [limit, offset], function (err, results) {
+            if (err) throw `Database error: ${err}`
+
+            callback(results.rows)
+        })
     }
 
 }
